2fd7f37ea8ec877fae7fb9b939c0d0b80bb31398
davidpacco <david.j.pacco@gmail.com>
2024-08-04 19:31:57 -0500

    Add component to create a person

---
+++ src/components/PersonForm.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react"
+import { gql, useMutation } from "@apollo/client"
+
+const CREATE_PERSON = gql`
+  mutation createPerson($name: String!, $phone: String, $street: String!, $city: String!) {
+    addPerson(
+      name: $name
+      phone: $phone
+      street: $street
+      city: $city
+    ) {
+      id
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+export function PersonForm() {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+
+  const [createPerson] = useMutation(CREATE_PERSON)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    createPerson({ variables: { name, phone, street, city } })
+
+    setName('')
+    setPhone('')
+    setStreet('')
+    setCity('')
+  }
+
+  return (
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Phone
+            <input
+              type="text"
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Street
+            <input
+              type="text"
+              onChange={(e) => setStreet(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            City
+            <input
+              type="text"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
+}
